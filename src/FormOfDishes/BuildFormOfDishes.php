49fafa67cebb1eb6ab2c8d4ff5a46fc6179d3158
Igor <ig@c-ua.com>
2018-05-24 16:15:27 +0300

    Added functionality

---
+++ src/FormOfDishes/BuildFormOfDishes.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: UserCE
+ * Date: 24.05.2018
+ * Time: 15:59
+ */
+
+namespace Softce\Rangeofdishes\FormOfDishes;
+
+use Mage2\Ecommerce\Models\Database\Package;
+use Softce\Rangeofdishes\FormOfDishes\Contract\FormOfDishes;
+
+class BuildFormOfDishes implements FormOfDishes
+{
+
+    public function buildForm()
+    {
+        $packages = Package::with('products')->get();
+
+        dump($packages);
+    }
+}
---
+++ src/FormOfDishes/Contract/FormOfDishes.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: UserCE
+ * Date: 24.05.2018
+ * Time: 16:00
+ */
+namespace Softce\Rangeofdishes\FormOfDishes\Contract;
+
+use \Illuminate\Support\Collection;
+
+interface FormOfDishes
+{
+    public function buildForm();
+}
